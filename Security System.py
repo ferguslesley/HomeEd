4aacdfdefacc8b862db788f7e1944e093d800898
ferguslesley <ferguslesley@gmail.com>
2016-07-13 09:57:10 +0100

    Wrapped in Main function

--- Security System.py
+++ Security System.py
@@ -1,10 +1,12 @@
 # Ferg's Personal Security System
-print("Name Verification Please.")
-name = input("> ")
+def main():
+    print("Name Verification Please.")
+    name = input("> ")
 
-if name == "Fergus":
-    print("Verification Accepted, Welcome, Fergus.")
-else:
-    for t in range(0,10):
-        print("INTRUDER ALERT. LEAVE NOW!!!")
-    
+    if name == "Fergus":
+        print("Verification Accepted, Welcome, Fergus.")
+    else:
+        for t in range(0,10):
+            print("INTRUDER ALERT. LEAVE NOW!!!")
+        
+main()
