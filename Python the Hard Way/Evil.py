e1d534f0388b8a25d56082c1a565fff71d750d8b
Fergus Lesley <ferguslesley@gmail.com>
2016-09-13 10:37:49 +0100

    Rescued from obelix

---
+++ Python the Hard Way/Evil.py
@@ -0,0 +1,7 @@
+#This will not be shown in the terminal mwuhahahahahaha
+#YOU WILL NOT SEE THIS
+#I COULD BE PLANNING WORLD TAKEOVER
+#AND YOU WOULD NOT KNOW
+print "There is nothing strange going on in this file"
+#HAHAHAHA YOU ARE OBLIVIOUS TO MY POWAAAAAAAAAR
+print "Have a nice day!"
