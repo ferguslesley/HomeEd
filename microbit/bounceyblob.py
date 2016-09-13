956970d6e2f5fce3cb89830857e46025d28f11fd
Fergus Lesley <ferguslesley@gmail.com>
2016-09-13 11:26:52 +0100

    added y dimension. simplified to 1 loop

--- microbit/bounceyblob.py
+++ microbit/bounceyblob.py
@@ -3,21 +3,40 @@ from microbit import *
 xpos = 1
 maxx = 4
 minx = 0
-oldpos = 0
+oldposx = 0
+oldposy = 0
+xstep = 1
+ystep = 1
+ypos = 0
+maxy = 4
+miny = 0
 
-display.set_pixel(xpos,2,8)
-while True:
-    while xpos < maxx:
-        display.set_pixel(xpos,2,8)
-        display.set_pixel(oldpos,2,0)
+
+for i in range(1, 100):
+        display.set_pixel(xpos,ypos,8)
+        display.set_pixel(oldposx,oldposy,0)
+        
+        # move coords by step
+        oldposx = xpos
+        oldposy = ypos
+        xpos += xstep
+        ypos += ystep
+        #check for edges
+
         sleep(100)
-        oldpos = xpos
-        xpos = xpos + 1
-    while xpos > minx:
-        display.set_pixel(xpos,2,8)
-        display.set_pixel(oldpos,2,0)
-        sleep(100)
-        oldpos = xpos
-        xpos = xpos - 1
-
-
+        
+        if xpos >= maxx:
+            # (change direction to "bounce")
+            xpos = maxx
+            xstep = -1
+        if xpos <= minx:
+            # (change direction to "bounce")
+            xstep = minx
+            xstep = 1
+        if ypos >= maxy:
+            ypos = maxy
+            ystep = - 1
+        if ypos <= miny:
+            ypos = miny
+            ystep = 1
+        print(xpos, xstep)
