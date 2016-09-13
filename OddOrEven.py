5110894bf12f93753052ff4f458d2bcb48cda5eb
Fergus Lesley <ferguslesley@gmail.com>
2016-09-13 10:39:33 +0100

    Rescued from obelix

---
+++ OddOrEven.py
@@ -0,0 +1,11 @@
+# Making "number" the number that the operator types in
+number = input("> ")
+
+# Checking if the number is even by using % to see if the number has a remainder when divided by 2
+
+if (number % 2) == 1:
+    print number, "Is Odd"
+else:
+    print number, "Is Even"
+
+# Prints Odd or Even depending on if the number has a remainder when divided by 2
